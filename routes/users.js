7f8251d1f4fec4d82355f9a0a8f85e84985497ae
d33zhou <d33zhou@outlook.com>
2022-01-08 22:22:01 +0000

    initial setup for 5 user routes

--- routes/users.js
+++ routes/users.js
@@ -11,14 +11,59 @@ const router  = express.Router();
 const userRouter = (db) => {
 
   // GET /users/favourites
+  router.get('/favourites', (req, res) => {
+    db.query('SELECT * FROM users;')
+      .then(res => {
+        res.json(res.rows);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
 
   // GET /users/contributions
+  router.get('/contributions', (req, res) => {
+    db.query('SELECT * FROM users;')
+      .then(res => {
+        res.json(res.rows);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
 
   // GET /users/login
+  router.get('/login', (req, res) => {
+    db.query('SELECT * FROM users;')
+      .then(res => {
+        res.json(res.rows);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
 
   // GET /users/logout
+  router.get('/logout', (req, res) => {
+    db.query('SELECT * FROM users;')
+      .then(res => {
+        res.json(res.rows);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
 
   // GET /users/register
+  router.get('/register', (req, res) => {
+    db.query('SELECT * FROM users;')
+      .then(res => {
+        res.json(res.rows);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  });
 
   return router;
 };
