a39bba51c13182ff744acee398ba763deca918af
d33zhou <d33zhou@outlook.com>
2022-01-08 22:24:09 +0000

    remove widgets router and related references

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
 const mapsRoutes = require("./routes/maps");
 
 // Redirections for route paths
@@ -45,7 +44,6 @@ app.use('/maps', mapsRoutes);
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
 app.use("/api/maps", mapsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
