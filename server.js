3041ecd5a617ce879f91c973ac90bed6f59ab030
d33zhou <d33zhou@outlook.com>
2022-01-09 00:16:02 +0000

    install body-parser dependency

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
 db.connect();
 
+// body parser set-up
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
