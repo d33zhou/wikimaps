267078d93cc5395e2ec55e90aa043177d60945fb
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-01-11 17:00:46 -0500

    Add favourites to db and change heart color

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,18 +3,20 @@ $(document).ready(function() {
   $('.heart-button').on('click',function() {
     const map_id = $(this).attr('data-map-id');
     console.log(map_id);
-    if ($(this).hasClass('fa-heart-fav')) {
-      $(this).removeClass('fa-heart-fav')
-
+    const icon = $(this).find('i')
+    if (icon.hasClass('fa-heart-fav')) {
+      icon.removeClass('fa-heart-fav');
+      console.log('trying to remove class');
     } else {
-      $(this).addClass('fa-heart-fav')
+      icon.addClass('fa-heart-fav');
+      console.log('trying to add class');
       $.ajax({
         url:"http://localhost:8080/users/favourites",
         method:"POST",
         data:{map_id:map_id}
       })
         .then(function(data) {
-
+          console.log(data);
         });
 
     }
