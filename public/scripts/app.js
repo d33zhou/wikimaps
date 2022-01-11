e4a91ea30f1af39c9c040976516bfb0fb57a5ee1
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-01-11 16:36:15 -0500

    Post request to favorites

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,26 @@
 // Client facing scripts here
 $(document).ready(function() {
-  for (let i = 0; i <= 2; i++) {
-    $(`.heart-button-${[i]}`).on("click",function() {
-      $(`#heart-${i}.fa-heart`).addClass('fa-heart-click');
-    });
-  }
+  $('.heart-button').on('click',function() {
+    const map_id = $(this).attr('data-map-id');
+    console.log(map_id);
+    if ($(this).hasClass('fa-heart-fav')) {
+      $(this).removeClass('fa-heart-fav')
+
+    } else {
+      $(this).addClass('fa-heart-fav')
+      $.ajax({
+        url:"http://localhost:8080/users/favourites",
+        method:"POST",
+        data:{map_id:map_id}
+      })
+        .then(function(data) {
+
+        });
+
+    }
+  });
+
+
 });
 
 
