1b46533907fde01971e24a8555b46e6ba2e1954f
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2022-01-11 12:49:49 -0500

    Favourites button should reference dbto determine color

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,2 +1,10 @@
 // Client facing scripts here
+$(document).ready(function() {
+  for (let i = 0; i <= 2; i++) {
+    $(`.heart-button-${[i]}`).on("click",function() {
+      $(`#heart-${i}.fa-heart`).addClass('fa-heart-click');
+    });
+  }
+});
+
 
