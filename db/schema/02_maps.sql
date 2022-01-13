f4a4d2716fe7fb7f8da484e70df9906075d6f356
d33zhou <d33zhou@outlook.com>
2022-01-13 15:56:33 +0000

    update psql maps table to include lat/long coords and location (i.e. city)

--- db/schema/02_maps.sql
+++ db/schema/02_maps.sql
@@ -4,5 +4,8 @@ CREATE TABLE maps (
   id SERIAL PRIMARY KEY NOT NULL,
   creator_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
-  description TEXT NOT NULL
+  description TEXT NOT NULL,
+  latitude NUMERIC NOT NULL,
+  longitude NUMERIC NOT NULL,
+  location VARCHAR (50)
 );
